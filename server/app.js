b9c3d496836d9c37f2d80ae79c1cee0bc0f5800e
Félix Romero <felix.fs3d@gmail.com>
2024-07-29 18:06:01 +0200

    Final dia 1

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
+const cors = require("cors")
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
@@ -20,7 +22,25 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({
 
+  origin:[
+    
+"http://localhost:5173",
+"http://localhost:5173/dashboard",
+"http://localhost:5173/students",
+"http://localhost:5173/cohort/1",
+"http://localhost:5173/cohort/1/edit",
+"http://localhost:5173/cohort/create",
+"http://localhost:5173/student/1",
+"http://localhost:5173/student/1/edit",
+"http://localhost:5173/signup",
+"http://localhost:5173/login",
+"http://localhost:5173/profile",
+
+]
+
+}))
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -29,7 +49,14 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+ app.get("/api/cohorts",  (req, res) => {
 
+res.json(cohorts)
+
+ })
+ app.get("/api/students", (req, res)=>{
+  res.json(students)
+ })
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
